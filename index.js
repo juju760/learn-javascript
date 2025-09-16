246db5ff62c563ef3371c39ecb28ecd993a61499
juju760 <juju@nhis.or.kr>
2025-09-16 09:55:04 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const username ="박혜죵"; //const = 상수. 변할 수 없는 값 
+console.log("🚀 ~ username:", username)
+console.log("🚀 ~ username:", username)
+console.log("🚀 ~ username:", username);
+let usermbti = "ESFJ"; //let = 변수. 변할 수 있는 값 
+console.log("🚀 ~ usermbti:", usermbti);
+
+
+console.log("USERNAME은? "+username);
+console.log("MBTI는? "+usermbti);
+
+usermbti = "ISTJ";
+console.log("🚀 ~ usermbti:", usermbti);
+
+// 상수는 변경 불가능 
+// username = "송주령";
+// console.log("🚀 ~ username:",username );
