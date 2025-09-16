96f0636b36a226b1b4bba6b386233bb5d58ac04c
juju760 <juju@nhis.or.kr>
2025-09-16 11:45:00 +0900

    함수_멀티캠퍼스

--- index.js
+++ index.js
@@ -37,3 +37,64 @@ undifined: 개발자 의도와 상관없이 값이 없는 상태
 */
 
 let emp;
+
+// 함수
+
+console.log("=-----------------------------------------");
+
+// 함수 정의 / 선언
+function greeting(){
+  //실행할 로직 
+  console.log("HI~♥");
+  console.log("반갑습니다. " + username +"님♡");
+
+  //백틱
+  console.log(`반갑습니다 ${username} 당신의 MBTI는 ${usermbti}이시군요.`);
+}
+
+//function greeting2 // 기본구문 
+// 아래는 화살표 함수 
+const greeting2 = () => {
+  //실행할 로직 
+  console.log("HI~♥");
+  console.log("반갑습니다. " + username +"님♡");
+
+  //백틱
+  console.log(`반갑습니다 ${username} 당신의 MBTI는 ${usermbti}이시군요.`);
+}
+
+
+// 함수 호출 / 실행 
+greeting();
+greeting2();
+
+//함수의 인자와 매개변수 
+//매개변수: 정의하는 부분에서
+//인자: 호출하는 부분에서 
+const sum = (num1, num2) => {
+  //실행할 로직 
+  const result = num1 + num2;
+  console.log("🚀 ~ sum ~ result:", result)
+  return result;
+
+  console.log("return 끝!!!");
+}
+
+const add = sum(20,7);
+console.log("🚀 ~ add:", add)
+
+
+//미션: sayHello라는 함수 만들기
+//사용자 이름을 받아서 반갑습니다 --- 님 
+const sayHello = (uname) => {
+  console.log("반갑습니다. "+uname +"님");
+}
+
+sayHello("인기스타 송상훈");
+
+const double = (num) => {
+  return num * 2
+}
+
+const doubleResult = double(add);
+console.log("🚀 ~ doubleResult:", doubleResult)
